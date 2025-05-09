18133c4b4242a4ff975152025c486c2440a7b430
Ruaww06 <2985564406@qq.com>
2025-05-09 22:19:14 +0800

    修复小bug

--- utils.py
+++ utils.py
@@ -12,7 +12,8 @@ def generate_xiaohongshu(theme, deepseek_api_key):
         ("human", user_template_text)
     ])
 
-    model = ChatDeepSeek(model="deepseek-chat")
+    model = ChatDeepSeek(model="deepseek-chat",api_key=deepseek_api_key,base_url="https://api.deepseek.com")
+
 
     output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
     chain = prompt | model | output_parser
